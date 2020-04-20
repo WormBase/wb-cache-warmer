d37cb8d07f9deb36f0004c85cbbd4e3d7f9ccc88
Sibyl <sibyl@wormbase.org>
2020-04-20 17:11:18 +0000

    swap order of slow pattern

--- src/wb_cache_warmer/core.clj
+++ src/wb_cache_warmer/core.clj
@@ -45,22 +45,25 @@
          ident-attr)))
 
 (def slow-path-patterns
-  ["/rest/widget/gene/{id}/interactions"
-   "/rest/widget/gene_class/{id}/current_genes"
+  ["/rest/widget/gene_class/{id}/current_genes"
    "/rest/widget/gene_class/{id}/previous_genes"
-   "/rest/widget/interaction/{id}/interactions"
    "/rest/widget/molecule/{id}/affected"
    "/rest/widget/phenotype/{id}/rnai"
    "/rest/widget/phenotype/{id}/variation"
    "/rest/widget/strain/{id}/contains"
    "/rest/widget/transposon_family/{id}/var_motifs"
-   "/rest/widget/wbprocess/{id}/interactions"
+
+   "/rest/widget/gene/{id}/interactions"
    "/rest/field/gene/{id}/interaction_details"
    "/rest/field/gene/{id}/interactions"
+
+   "/rest/widget/interaction/{id}/interactions"
    "/rest/field/interaction/{id}/interaction_details"
-   ;"/rest/field/interaction/{id}/interactions"
+   ;;"/rest/field/interaction/{id}/interactions"
+   "/rest/widget/wbprocess/{id}/interactions"
    "/rest/field/wbprocess/{id}/interaction_details"
-   "/rest/field/wbprocess/{id}/interactions"])
+   "/rest/field/wbprocess/{id}/interactions"
+   ])
 
 (defn schedule-jobs-all [db hostname]
   ;; schedule something for testing
